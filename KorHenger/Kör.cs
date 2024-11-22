0267a1af876910f5c9a8e762f13f923955591fd2
TZoltan01 <topcsiovzoltan0402@gmail.com>
2024-11-22 11:58:44 +0100

    try cache

--- KorHenger/Kör.cs
+++ KorHenger/Kör.cs
@@ -3,76 +3,92 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
- 
+
 namespace KörHenger
 {
-    class Kör
+    public class Kör
     {
         protected double sugar,
             terület,
             kerület;
- 
+
         public Kör() { }
- 
+
         public Kör(double r)
         {
-            this.sugar = r;
+            if (r <= 0)
+            {
+                throw new ArgumentException("A kör sugarának nagyobbnak kell lennie mint 0!");
+            }
+            else this.sugar = r;
         }
- 
+
         public void SetSugar(double r)
         {
-            this.sugar = r;
+            if (r <= 0)
+            {
+                throw new ArgumentException("A kör sugarának nagyobbnak kell lennie mint 0!");
+            }
+            else this.sugar = r;
         }
- 
+
         public void SetTerület()
         {
             this.terület = Math.Round(Math.Pow(this.sugar, 2) * Math.PI, 2);
         }
- 
+
         public void SetKerület()
         {
             this.kerület = Math.Round(2 * this.sugar * Math.PI, 2);
         }
- 
+
         public double GetTerület()
         {
             return this.terület;
         }
- 
+
         public double GetKerület()
         {
             return this.kerület;
         }
- 
+
         public double GetSugár()
         {
             return this.sugar;
         }
     }
- 
+
     class Henger : Kör
     {
         // Osztályváltozók
         private double térfogat,
             magasság;
- 
+
         // Konstruktor
         public Henger(double s, double m)
         {
             // Változók beállítása
-            this.sugar = s;
-            this.magasság = m;
- 
-            // Számítások elvégzése
-            SetKerület();
-            SetTerület();
-            this.térfogat = this.terület * this.magasság;
+            if (s <= 0 || m <= 0) 
+            {
+                throw new ArgumentException("A henger sugarának és magasságának nagyobbnak kell lennie mint 0!");
+            }
+            else
+            {
+                this.sugar = s;
+                this.magasság = m;
+
+                // Számítások elvégzése
+                SetKerület();
+                SetTerület();
+                this.térfogat = this.terület * this.magasság;
+            }
+
         }
- 
+
         public double GetTérfogat()
         {
             return this.térfogat;
         }
- 
+
     }
 }
--- KorHenger/Program.cs
+++ KorHenger/Program.cs
@@ -16,21 +16,57 @@ namespace KörHenger
             k1.SetTerület();
             k1.SetKerület();
  
-            kiír(k1);
+            // kiír(k1);
  
             // 2. példány
             Kör k2 = new Kör(58);
             k2.SetKerület();
             k2.SetTerület();
+
+            // kiír(k2);
+
+            // 1. Henger
+
+            try
+            {
+                Henger h1 = new Henger(15, 34);
+                kiírHenger(h1);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine($"Aritmetikai hiba {e.Message}!");
+            }
+
+            // 2.Henger
+            try
+            {
+                Henger h2 = new Henger(-1, 30);
+                kiírHenger(h2);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
  
-            kiír(k2);
- 
+
+
+
             Console.ReadKey();
         }
  
         private static void kiír(Kör k)
         {
             Console.WriteLine($"A {k.GetSugár()} sugarú kör kerülete: {k.GetKerület()}, területe: {k.GetTerület()}.");
+        }
+
+        private static void kiírHenger(Henger k)
+        {
+            Console.WriteLine($"A {k.GetSugár()} sugarú henger kerülete: {k.GetKerület()}, területe: {k.GetTerület()}, térfogat: {k.GetTérfogat()}.");
         }
     }
 }
